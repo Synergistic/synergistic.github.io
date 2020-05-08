ba768fba67cb470595a918628fbb4850f38940dd
Dan <Synergistic@users.noreply.github.com>
2020-05-08 14:57:07 -0400

    Change app to ACNL

---
+++ precache-manifest.7886a0e4b4184f93614168e835cbbe3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a58c2701d6fbcdf62b81ea189923d216",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b07ad6b3fd0d5cf3c38f",
+    "url": "/static/css/main.df6b6fad.chunk.css"
+  },
+  {
+    "revision": "626be74d030fabf6557e",
+    "url": "/static/js/2.8b6cae29.chunk.js"
+  },
+  {
+    "revision": "12e41d8f132b1d17c29409b17e6b8cc5",
+    "url": "/static/js/2.8b6cae29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b07ad6b3fd0d5cf3c38f",
+    "url": "/static/js/main.561dfdf8.chunk.js"
+  },
+  {
+    "revision": "a7e339f4326c2e7f6561",
+    "url": "/static/js/runtime-main.89cf6e73.js"
+  }
+]);
