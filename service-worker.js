bd2695c73682991fc1c235719d37a8d551fa4892
Dan <Synergistic@users.noreply.github.com>
2020-05-08 15:24:29 -0400

    remove delete functionality

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.497053b31f3f61bbbb5f82b68b001628.js"
+  "/precache-manifest.2844defc3b774f075950df6e6f71192d.js"
 );
 
 self.addEventListener('message', (event) => {
