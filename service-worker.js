ad59e6d7a205679227f98c5a399da87c2d664516
Dan <Synergistic@users.noreply.github.com>
2020-05-10 16:28:34 -0400

    update visuals, add abilit to add items when there are results, add back in deletion with confirmation

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.65eb591b338153b1a902553a6041f02e.js"
+  "/precache-manifest.be35abb317d60f96f4b138cfc16ffb53.js"
 );
 
 self.addEventListener('message', (event) => {
